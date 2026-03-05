148611637ecb26602d3f75f705701911fadaea27
xurxo77 <xurxoac@gmail.com>
2026-03-05 14:18:02 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'taller-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/LOGOTALLER.png',
+    '/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
